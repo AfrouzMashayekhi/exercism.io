799fdf483e63fde126f0a525734b7407dfa91981
amash <mashayekhiafrouz@gmail.com>
2020-05-15 13:17:09 +0430

    add two fer bash

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# share prints two fer
+share() {
+  name="you"
+  if [ $# -ne 0 ]; then
+      name=$1
+  fi
+  echo "One for $name, one for me."
+}
+
+main () {
+ share "$@"
+}
+
+# call main with all of the positional arguments
+main "$@"
+
