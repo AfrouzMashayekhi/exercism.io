68a1034a1b89514000df9206ec40c226d1cbbd6f
amash <mashayekhiafrouz@gmail.com>
2020-05-16 13:14:00 +0430

    solve space age go track

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,3 +1,25 @@
+// Package space implement multi planet years
 package space
 
+// Planet shows which planet we live
 type Planet string
+
+// earthSeconds shows one period of a year in sec
+const earthSeconds = 31557600
+
+// Age how old we are in that planet
+func Age(seconds float64, planet Planet) float64 {
+	// period map of planet to period
+	period := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1.0,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
+	}
+
+	return seconds / (period[planet] * earthSeconds)
+}
