b94c90490940bad0e754c00a14d9f95dd82f9be6
amash <mashayekhiafrouz@gmail.com>
2020-05-21 20:57:12 +0430

    solve raindrops go track

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,1 +1,23 @@
+// Package raindrops converts digits to raindrops sound
 package raindrops
+
+import "strconv"
+
+// Convert converts a digit to a raindrop sound
+func Convert(num int) string {
+	// raindropsSound a string of raindrops
+	var raindropsSound string
+	if num%3 == 0 {
+		raindropsSound += "Pling"
+	}
+	if num%5 == 0 {
+		raindropsSound += "Plang"
+	}
+	if num%7 == 0 {
+		raindropsSound += "Plong"
+	}
+	if raindropsSound == "" {
+		raindropsSound = strconv.Itoa(num)
+	}
+	return raindropsSound
+}
