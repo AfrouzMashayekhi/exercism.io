ecbfd3a72721c4935f6136467a255ec7e1b906e4
amash <mashayekhiafrouz@gmail.com>
2020-05-22 18:57:20 +0430

    solved triangle go track

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,6 +1,8 @@
 // Package trianle detects which trianle is
 package triangle
 
+import "math"
+
 // Kind a type for your triangle
 type Kind string
 
@@ -15,6 +17,20 @@ const (
 // KindFromSides detects triangle type and return its kind
 func KindFromSides(a, b, c float64) Kind {
 	var k Kind
+	if a+b+c <= 0 || math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) || math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		k = NaT
+	} else {
+		switch {
+		case a == b && b == c:
+			k = Equ
+		case a > b+c || b > a+c || c > b+a:
+			k = NaT
+		case a == b || b == c || a == c:
+			k = Iso
+		default:
+			k = Sca
+		}
+	}
 
 	return k
 }
