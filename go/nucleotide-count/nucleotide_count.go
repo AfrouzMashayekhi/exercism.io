b0ec0322e24c4d07130e25836eef1f53f452fe47
amash <mashayekhiafrouz@gmail.com>
2020-05-25 00:38:32 +0430

    solve nucleotides counts go track

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -3,25 +3,27 @@ package dna
 
 import (
 	"errors"
-	"strings"
 )
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-type Histogram map[DNA]int
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
 
-const dna string = "CGAT"
-
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
 func (d DNA) Counts() (Histogram, error) {
-
-	var h Histogram
+	// rune can not be empty
+	h := Histogram{
+		'C': 0,
+		'G': 0,
+		'T': 0,
+		'A': 0,
+	}
 	for _, nucleotides := range d {
-		if strings.Contains(dna, string(nucleotides)) {
-			h[DNA(nucleotides)] += 1
+		if _, ok := h[nucleotides]; ok {
+			h[nucleotides] += 1
 		} else {
 			return nil, errors.New("not a nucleontides of DNA")
 		}
