fee111216c3f6265c67dc95c690a61d7aaa84860
amash <mashayekhiafrouz@gmail.com>
2020-05-25 01:30:35 +0430

    solve accumulate  go track

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -1,1 +1,12 @@
+// Package accumulate operates some functions on strings
 package accumulate
+
+// Accumulate converts str to a string depend on input operator
+func Accumulate(str []string, converter func(string) string) []string {
+	// Accumulated returns array of string that operator applied
+	var accumulated []string
+	for _, words := range str {
+		accumulated = append(accumulated, converter(words))
+	}
+	return accumulated
+}
